e5ec5eedcce133d6a45ebdb4892c7f9bef312525
Robert Driskell <driskell.ba@gmail.com>
2019-04-10 17:41:05 -0500

    Animated articles

--- Article/Article.js
+++ Article/Article.js
@@ -7,7 +7,7 @@ class Article {
     // create a reference to the ".expandButton" class. 
     this.expandButton = this.domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = 'expand';
+    this.expandButton.textContent = 'Click to Expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandArticle = this.expandArticle.bind(this);
     this.expandButton.addEventListener('click', this.expandArticle);
@@ -16,11 +16,11 @@ class Article {
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.domElement.classList.toggle('article-open');
-    if(this.expandButton.textContent == 'close') {
-      this.expandButton.textContent = 'expand';
+    if(this.expandButton.textContent == 'Click to Close') {
+      this.expandButton.textContent = 'Click to Expand';
     }
     else {
-      this.expandButton.textContent = 'close';
+      this.expandButton.textContent = 'Click to Close';
     }
   }
 }
